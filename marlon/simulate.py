2ac300a0b06fb7fab031ed3721ca81cc71e3b92a
Christian-Fisher <christian.fisher@carleton.ca>
2021-11-26 21:42:28 +0000

    Re-added the break when the done flag is set.
    Signed-off-by: Christian-Fisher <christian.fisher@carleton.ca>

--- marlon/simulate.py
+++ marlon/simulate.py
@@ -16,7 +16,7 @@ def generate_graph_json(cyberbattle_env: CyberBattleEnv, iteration):
 
 def run_simulation(iteration_count):
     # Load the Gym environment
-    gymid = "CyberBattleToyCtf-v0"
+    gymid = "CyberBattleChain-v0"
     gym_env = gym.make(gymid)
 
     maximum_node_count = 12
@@ -51,6 +51,7 @@ def run_simulation(iteration_count):
         # If there is a jump in the reward for this step, record it for UI display.
         if done:
             simulation.append(generate_graph_json(gym_env, iteration_count))
+            break
         if reward != 0 or iteration == iteration_count-1:
             simulation.append(generate_graph_json(gym_env, iteration+1))  
 
