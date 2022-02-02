f505dc6e0a5b293f375ebe8181c51a924694d3b9
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2022-02-02 16:58:32 +0000

    chore(a2c): Convert train script to use MultiAgentUniverse

--- marlon/baseline_models/a2c/train.py
+++ marlon/baseline_models/a2c/train.py
@@ -1,36 +1,37 @@
-import gym
-
-import cyberbattle
 from stable_baselines3 import A2C
-from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.monitor import Monitor
 
-from marlon.baseline_models.env_wrappers.attack_wrapper import AttackerEnvWrapper
-from marlon.baseline_models.ppo.eval import evaluate
+from marlon.baseline_models.multiagent.baseline_marlon_agent import BaselineAgentBuilder
+from marlon.baseline_models.multiagent.multiagent_universe import MultiAgentUniverse
 
 ENV_MAX_TIMESTEPS = 2000
 LEARN_TIMESTEPS = 10_000
 LEARN_EPISODES = 1000 # Set this to a large value to stop at LEARN_TIMESTEPS instead.
 ENABLE_ACTION_PENALTY = True
+EVALUATE_EPISODES = 5
 
 def train(evaluate_after=False):
-    env_id = "CyberBattleToyCtf-v0"
-    env = gym.make(env_id)
+    universe = MultiAgentUniverse.build(
+        env_id='CyberBattleToyCtf-v0',
+        attacker_builder=BaselineAgentBuilder(
+            alg_type=A2C,
+            policy='MultiInputPolicy'
+        ),
+        attacker_enable_action_penalty=ENABLE_ACTION_PENALTY
+    )
 
-    env = AttackerEnvWrapper(
-        env,
-        max_timesteps=ENV_MAX_TIMESTEPS,
-        enable_action_penalty=ENABLE_ACTION_PENALTY)
+    universe.learn(
+        total_timesteps=LEARN_TIMESTEPS,
+        n_eval_episodes=LEARN_EPISODES
+    )
 
-    check_env(env)
-
-    model = A2C('MultiInputPolicy', Monitor(env), verbose=1)
-    model.learn(total_timesteps=LEARN_TIMESTEPS, n_eval_episodes=LEARN_EPISODES)
-
-    model.save('a2c.zip')
+    universe.save(
+        attacker_filepath='a2c.zip'
+    )
 
     if evaluate_after:
-        evaluate(max_timesteps=ENV_MAX_TIMESTEPS)
+        universe.evaluate(
+            n_episodes=EVALUATE_EPISODES
+        )
 
 if __name__ == '__main__':
     train(evaluate_after=True)
