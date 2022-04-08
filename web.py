77539d65ff14c5682b4c8634dd0cc59d88115127
Chrisnguyen3 <chrisnguyen3@cmail.carleton.ca>
2022-04-08 12:07:02 -0400

    feat: Attacker and Defender IDs

--- web.py
+++ web.py
@@ -9,8 +9,8 @@ SIMULATION = SimulationCache()
 @app.route('/')
 def home():
     if SIMULATION.value is None:
-        return render_template('index.html', max_sim=0, num_graphs=0, graphs=[])
-    return render_template('index.html', max_sim=ITERATION_COUNT, num_graphs=len(SIMULATION.value), graphs=[])
+        return render_template('index.html', max_sim=0, num_graphs=0, graphs=[], attacker_name = "-", defender_name = "-")
+    return render_template('index.html', max_sim=ITERATION_COUNT, num_graphs=len(SIMULATION.value), graphs=[], attacker_name = "-", defender_name = "-")
 
 @app.route('/upload-file', methods=['POST'])
 def upload_file():
@@ -33,8 +33,16 @@ def upload_file():
         defender_file=defender_file
     )
 
+    attacker_name = attacker_option
+    if attacker_name == "Load":
+        attacker_name = attacker_file
+    
+    defender_name = defender_option
+    if defender_name == "Load":
+        defender_name = defender_file
+
     graphs = json.dumps(SIMULATION.value, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('index.html', max_sim=ITERATION_COUNT, num_graphs=len(SIMULATION.value), graphs=graphs)
+    return render_template('index.html', max_sim=ITERATION_COUNT, num_graphs=len(SIMULATION.value), graphs=graphs, attacker_name = attacker_name, defender_name = defender_name)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000)
